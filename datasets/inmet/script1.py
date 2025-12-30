b62362ab75cd79da5d40e6f42f388d394978e5f3
bzamith <bruna.zamith@hotmail.com>
2025-12-30 12:03:31 -0300

    Increase INMET datasets to cover 2025

--- datasets/inmet/script1.py
+++ datasets/inmet/script1.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 
-inputdir = '/Users/zamith/Downloads/2007'
+inputdir = '/Users/zamith/Downloads/2025'
 
 for filename in os.listdir(inputdir):
 	if filename.endswith('.CSV'):
